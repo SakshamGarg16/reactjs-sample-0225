7475a1fe4b98056058432b47189b217b2fe1abfa
Saksham Garg <22052500@kiit.ac.in>
2025-06-02 18:13:32 +0530

    Updated profile section

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,25 @@
+'use client';
+
 import ProtectedRoute from '../../components/ProctedRoute';
+import { useEffect, useState } from 'react';
 
 export default function ProfilePage() {
+  const [pic, setPic] = useState('');
+
+  useEffect(() => {
+    const rand = Math.floor(Math.random() * 1000);
+    setPic(`https://picsum.photos/id/${rand}/200`);
+  }, []);
   return (
     <ProtectedRoute>
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-6">Profile</h1>
-        <p>This is your profile page.</p>
-        {/* Your profile content here */}
-      </div>
+      <main className="p-6 text-center">
+      <h1 className="text-3xl font-bold mb-4">Profile</h1>
+      {pic ? (
+        <img src={pic} alt="profile" className="w-32 h-32 rounded-full mx-auto" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </main>
     </ProtectedRoute>
   );
 }
